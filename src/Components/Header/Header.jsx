316834738847b72d75ddc0fbbfa00c5e2fcc0292
abusayeds <abusayedstudent855@gmail.com>
2023-11-05 02:15:24 +0600

    responsive bottom nagigation added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,14 +5,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import './Header.css'
 import { Link } from 'react-router-dom';
-import { faAngleDown, faBars,faX } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faBars,faBorderNone,faHome,faSearch,faShoppingCart,faUser,faX } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 const Header = () => {
   const [open , setOpen ] =useState(false)
 
    return (
-    <main className='  header text-black '>
-    <div className=' bg-red-600 md:w-5/6 mx-auto flex justify-between p-2 md:rounded  '>
+    <section className=' header'>
+      <main className=' text-black md:w-5/6  md:mx-auto  '>
+    <div className=' bg-red-600   md:flex hidden justify-between p-2 md:rounded  '>
 
     <div className=' w-12 flex items-center'>
       <img className='rounded' src={img} alt=""  />
@@ -24,28 +25,45 @@ const Header = () => {
     </div>
 
     </div>
+
+
+
+
+   
+
+   
+
+
+
+
+    
      
-     <nav className='bg-slate-200 md:w-5/6 md:m-auto  '>
-         <div className='md:flex md:items-center md:justify-center '>
+     <nav className='bg-slate-200  '>
+         <div className=' md:flex items-center justify-around '>
             
-            <div className='flex justify-between p-4 ' >
+            <div className='  flex justify-between p-2 ' >
            
               
-              <input className=' p-2 rounded ' type="text" placeholder='search foods ' />
-         
-              <p onClick={() => setOpen(!open)} className='text-2xl md:hidden'>
+              <div className=' relative flex items-center'>
+              <input className=' px-2  py-2 rounded ' type="text" placeholder='search foods  ' onFocus={faBorderNone} />
+               <FontAwesomeIcon className='absolute right-3' icon={faSearch}></FontAwesomeIcon>
+              </div>
+               
+               <div>
+               <p onClick={() => setOpen(!open)} className='text-2xl md:hidden'>
                
                
-                {
-                  open ?  <FontAwesomeIcon icon={faX}></FontAwesomeIcon> 
-                  : <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
-                }
-                
-                </p>
-              
+               {
+                 open ?  <FontAwesomeIcon icon={faX}></FontAwesomeIcon> 
+                 : <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+               }
+               
+               </p>
+             
+               </div>
             </div>
 
-           <div className='md:flex hidden ml-10 items-center'>
+           <div className='md:flex hidden ml-10  '>
             
                   <div className='group'>
                     <p><Link to="" className='py-7 px-4 text-black  '> category <FontAwesomeIcon icon={faAngleDown}></FontAwesomeIcon></Link></p>
@@ -61,15 +79,16 @@ const Header = () => {
                   <Link to="" className='py-7 px-4 text-black'> Inventory</Link>
                   <Link to="" className='py-7 px-4 text-black '>Servise</Link>
                   </div>
-            
-
-            
            </div>
+           <div className='md:flex hidden'>
+              <FontAwesomeIcon className='text-red-500 text-xl' icon={faShoppingCart}></FontAwesomeIcon>
+              
+            </div>
 
            {/*------- mobail devise -----*/}
 
            <div className={`md:hidden bg-slate-200 w-full h-full relative bottom-0  duration-500  ${open ? "left-0" : "hidden"} } `}>
-                  <div className='px-6 py-4 flex gap-3'>
+                  <div className='px-2 py-4 flex gap-3'>
                               
                 
                   <div className='group'>
@@ -103,6 +122,19 @@ const Header = () => {
      </nav>
 
     </main>
+
+    <main className= 'nav-sm  bg-red-600   flex md:hidden justify-between px-10 py-3 md:rounded '>
+       <div>
+       <Link to='/' className='text-white text-2xl'><FontAwesomeIcon icon={faHome}></FontAwesomeIcon></Link> 
+       </div>
+       <div>
+       <Link className='text-white text-2xl'><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></Link>
+       </div>
+       <div>
+       <Link className='text-white text-2xl'><FontAwesomeIcon icon={faUser}></FontAwesomeIcon></Link>
+       </div>
+    </main>
+    </section>
   );
 };
 
